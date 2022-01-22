fb40e3bad7a980723e7af53bbb385d3163d79dfe
David O.Silva <david2015os@hotmail.com>
2022-01-22 20:42:39 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,13 +21,38 @@ public class Main {
         mentoria.setTitulo("Mentoria Start UNI GFT.");
         mentoria.setDescricao("Programa de estágio GFT.");
         mentoria.setData(LocalDate.now());
-
-
-
-
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
-    }
+        */
+        Conteudo conteudo = new Curso();
+        Conteudo conteudo1 = new Mentoria();
 
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDavid = new Dev();
+        devDavid.setNome("David");
+        devDavid.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devDavid.getConteudosInscritos());
+        devDavid.progredir();
+        System.out.println("Conteúdos Concluidos" + devDavid.getConteudosConcluidos());
+        System.out.println("\nXP: " + devDavid.calcularTotalXp());
+
+        System.out.println("--------------------------------");
+
+        Dev devJaqueline = new Dev();
+        devJaqueline.setNome("João");
+        devJaqueline.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devJaqueline.getConteudosInscritos());
+        devJaqueline.progredir();
+        System.out.println("Conteúdos Concluidos" + devDavid.getConteudosConcluidos());
+        System.out.println("\nXP: " + devDavid.calcularTotalXp());
+    }
 }
